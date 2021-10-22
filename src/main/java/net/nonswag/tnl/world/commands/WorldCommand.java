7f138def470db7f82b61ba6a3fbb3722b4355b9e
NonSwag <mrminecraft00@gmail.com#>
2021-10-22 21:48:16 +0200

    update

--- src/main/java/net/nonswag/tnl/world/commands/WorldCommand.java
+++ src/main/java/net/nonswag/tnl/world/commands/WorldCommand.java
@@ -2,9 +2,10 @@ package net.nonswag.tnl.world.commands;
 
 import net.nonswag.tnl.core.api.command.CommandSource;
 import net.nonswag.tnl.core.api.command.Invocation;
-import net.nonswag.tnl.core.api.message.Message;
 import net.nonswag.tnl.core.api.message.Placeholder;
+import net.nonswag.tnl.core.api.message.key.MessageKey;
 import net.nonswag.tnl.listener.api.command.TNLCommand;
+import net.nonswag.tnl.listener.api.command.exceptions.SourceMismatchException;
 import net.nonswag.tnl.listener.api.player.TNLPlayer;
 import net.nonswag.tnl.world.api.Environment;
 import net.nonswag.tnl.world.api.WorldType;
@@ -155,12 +156,12 @@ public class WorldCommand extends TNLCommand {
                     World world = Bukkit.getWorld(args[1]);
                     if (world != null) {
                         world.save();
-                        source.sendMessage(Message.WORLD_SAVED_EN.getText(new Placeholder("world", world.getName())));
+                        source.sendMessage(MessageKey.WORLD_SAVED, new Placeholder("world", world.getName()));
                     } else source.sendMessage("%prefix% §c/world export §8(§6World§8)");
                 } else {
                     for (World world : Bukkit.getWorlds()) {
                         world.save();
-                        source.sendMessage(Message.WORLD_SAVED_EN.getText(new Placeholder("world", world.getName())));
+                        source.sendMessage(MessageKey.WORLD_SAVED, new Placeholder("world", world.getName()));
                     }
                 }
             } else if (args[0].equalsIgnoreCase("load")) {
@@ -168,11 +169,8 @@ public class WorldCommand extends TNLCommand {
                     World world = Bukkit.getWorld(args[1]);
                     if (world == null) {
                         world = WorldUtil.getInstance().loadWorld(args[1]);
-                        if (world != null) {
-                            source.sendMessage("%prefix% §7Loaded World§8: §6" + args[1]);
-                        } else {
-                            source.sendMessage("%prefix% §cFailed to load World §4" + args[1]);
-                        }
+                        if (world != null) source.sendMessage("%prefix% §7Loaded World§8: §6" + args[1]);
+                        else source.sendMessage("%prefix% §cFailed to load World §4" + args[1]);
                     } else source.sendMessage("%prefix% §cA world with this name already exist");
                 } else source.sendMessage("%prefix% §c/world load §8[§6World§8]");
             } else if (args[0].equalsIgnoreCase("setspawn")) {
@@ -181,13 +179,13 @@ public class WorldCommand extends TNLCommand {
                     player.getWorld().setSpawnLocation(player.getLocation());
                     Location l = player.getWorld().getSpawnLocation();
                     source.sendMessage("%prefix% §aSuccessfully set the spawn location to §6" + l.getX() + "§8, §6" + l.getY() + "§8, §6" + l.getZ());
-                } else source.sendMessage(Message.PLAYER_COMMAND_EN.getText());
+                } else throw new SourceMismatchException();
             } else if (args[0].equalsIgnoreCase("spawn")) {
                 if (source.isPlayer()) {
                     TNLPlayer player = (TNLPlayer) source.player();
                     player.teleport(player.getWorld().getSpawnLocation());
                     source.sendMessage("%prefix% §aTeleported you to the world spawn of world §6" + player.getWorld().getName());
-                } else source.sendMessage(Message.PLAYER_COMMAND_EN.getText());
+                } else throw new SourceMismatchException();
             } else if (args[0].equalsIgnoreCase("list")) {
                 List<String> worlds = new ArrayList<>();
                 Bukkit.getWorlds().forEach(world -> worlds.add(world.getName()));
