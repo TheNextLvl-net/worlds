b1eea601bad8a227b47fb01ca66dfab8e693df21
NonSwag <mrminecraft00@gmail.com#>
2022-05-26 10:43:42 +0200

    added environment based generation

--- src/main/java/net/nonswag/tnl/world/generators/SimplexOctaveGenerator.java
+++ src/main/java/net/nonswag/tnl/world/generators/SimplexOctaveGenerator.java
@@ -1,8 +1,9 @@
 package net.nonswag.tnl.world.generators;
 
 import lombok.Getter;
-import org.bukkit.*;
-import org.bukkit.block.Biome;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
 
 import javax.annotation.Nonnull;
@@ -39,16 +40,22 @@ public class SimplexOctaveGenerator extends CustomGenerator {
             @Nonnull
             @Override
             public ChunkData generateChunkData(@Nonnull World world, @Nonnull Random random, int chunkX, int chunkZ, @Nonnull BiomeGrid biome) {
-                org.bukkit.util.noise.SimplexOctaveGenerator generator = new org.bukkit.util.noise.SimplexOctaveGenerator(new Random(world.getSeed()), 8);
+                org.bukkit.util.noise.SimplexOctaveGenerator generator = new org.bukkit.util.noise.SimplexOctaveGenerator(world, 8);
                 ChunkData chunk = createChunkData(world);
                 generator.setScale(0.005D);
+                World.Environment environment = world.getEnvironment();
                 for (int x = 0; x < 16; x++) {
                     for (int z = 0; z < 16; z++) {
                         currentHeight = (int) (generator.noise(chunkX * 16 + x, chunkZ * 16 + z, 0.00001, 0.00001) * 3 + 64);
-                        chunk.setBlock(x, currentHeight, z, Material.GRASS_BLOCK);
-                        chunk.setBlock(x, currentHeight - 1, z, Material.DIRT);
-                        for (int i = currentHeight - 2; i > 0; i--) chunk.setBlock(x, i, z, Material.STONE);
-                        for (int y = 0; y < world.getMaxHeight(); y++) biome.setBiome(x, y, z, Biome.PLAINS);
+                        if (environment.equals(World.Environment.NORMAL)) {
+                            chunk.setBlock(x, currentHeight, z, Material.GRASS_BLOCK);
+                            chunk.setBlock(x, currentHeight - 1, z, Material.DIRT);
+                            for (int i = currentHeight - 2; i > 0; i--) chunk.setBlock(x, i, z, Material.STONE);
+                        } else if (environment.equals(World.Environment.THE_END)) {
+                            for (int i = currentHeight; i > 0; i--) chunk.setBlock(x, i, z, Material.END_STONE);
+                        } else if (environment.equals(World.Environment.NETHER)) {
+                            for (int i = currentHeight; i > 0; i--) chunk.setBlock(x, i, z, Material.NETHERRACK);
+                        }
                         chunk.setBlock(x, 0, z, Material.BEDROCK);
                     }
                 }
