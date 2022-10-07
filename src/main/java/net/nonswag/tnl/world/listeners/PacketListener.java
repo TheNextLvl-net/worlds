c9469fd2df2d2e5f6be514581e0ddcd9f4891b6b
NonSwag <mrminecraft00@gmail.com>
2022-10-07 23:17:19 +0200

    code cleanup

--- src/main/java/net/nonswag/tnl/world/listeners/PacketListener.java
+++ src/main/java/net/nonswag/tnl/world/listeners/PacketListener.java
@@ -1,6 +1,5 @@
 package net.nonswag.tnl.world.listeners;
 
-import net.minecraft.server.v1_16_R3.DimensionManager;
 import net.minecraft.server.v1_16_R3.PacketPlayOutRespawn;
 import net.nonswag.tnl.core.api.reflection.Reflection;
 import net.nonswag.tnl.listener.events.PlayerPacketEvent;
@@ -10,6 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import javax.annotation.Nonnull;
+import java.util.Objects;
 
 public class PacketListener implements Listener {
 
@@ -18,8 +18,7 @@ public class PacketListener implements Listener {
         if (!event.getDirection().isOutgoing()) return;
         if (!(event.getPacket() instanceof PacketPlayOutRespawn packet)) return;
         TNLWorld world = TNLWorld.cast(event.getPlayer().worldManager().getWorld());
-        DimensionManager manager = event.<DimensionManager>getPacketField("a").nonnull();
-        Reflection.setField(manager, "ambientLight", world.fullBright() ? 1 : 0);
+        Reflection.Field.set(Objects.requireNonNull(event.getPacketField("a")), "ambientLight", world.fullBright() ? 1 : 0);
         event.setPacketField("g", world.type().equals(WorldType.FLAT));
     }
 }
