639848a740b1bcac692da7fe76fcee7a6437c98d
NonSwag <54660361+NonSwag@users.noreply.github.com>
2023-05-03 21:29:49 +0200

    Delete src/main directory

--- src/main/java/net/nonswag/tnl/world/generators/package-info.java
+++
@@ -1,9 +0,0 @@
-@FieldsAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package net.nonswag.tnl.world.generators;
-
-import net.nonswag.core.api.annotation.FieldsAreNonnullByDefault;
-import net.nonswag.core.api.annotation.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
