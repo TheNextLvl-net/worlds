47978bdf2310362a838c2463f25eded241c50757
NonSwag <mrminecraft00@gmail.com>
2023-05-05 22:05:21 +0200

    added utility methods

--- api/src/main/java/net/thenextlvl/worlds/volume/Generator.java
+++ api/src/main/java/net/thenextlvl/worlds/volume/Generator.java
@@ -1,8 +1,30 @@
 package net.thenextlvl.worlds.volume;
 
+import org.bukkit.World;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.PluginClassLoader;
 import org.jetbrains.annotations.Nullable;
 
 public record Generator(String plugin, @Nullable String id) {
+
+    @Nullable
+    @SuppressWarnings("UnstableApiUsage")
+    public static Generator of(World world) {
+        if (world.getGenerator() == null) return null;
+        var loader = world.getGenerator().getClass().getClassLoader();
+        if (!(loader instanceof PluginClassLoader pluginLoader)) return null;
+        if (pluginLoader.getPlugin() == null) return null;
+        return new Generator(pluginLoader.getPlugin().getName(), null);
+    }
+
+    public static boolean isProvided(Class<? extends Plugin> clazz) {
+        try {
+            return clazz.getMethod("getDefaultWorldGenerator", String.class, String.class).getDeclaringClass().equals(clazz);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     @Override
     public String toString() {
