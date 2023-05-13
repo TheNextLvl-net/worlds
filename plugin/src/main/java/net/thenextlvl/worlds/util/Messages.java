8d7df716cdaf6eaf3ffbcf6d464e4c2ec1b2541b
NonSwag <mrminecraft00@gmail.com>
2023-05-13 16:08:53 +0200

    added click and hover event for seed

--- plugin/src/main/java/net/thenextlvl/worlds/util/Messages.java
+++ plugin/src/main/java/net/thenextlvl/worlds/util/Messages.java
@@ -80,7 +80,8 @@ public class Messages {
         file.setDefault(WORLD_INFO_TYPE, "%prefix% <gray>Type<dark_gray>: <white>%type%");
         file.setDefault(WORLD_INFO_ENVIRONMENT, "%prefix% <gray>Environment<dark_gray>: <white>%environment%");
         file.setDefault(WORLD_INFO_GENERATOR, "%prefix% <gray>Generator<dark_gray>: <white>%generator%");
-        file.setDefault(WORLD_INFO_SEED, "%prefix% <gray>Seed<dark_gray>: <white>%seed%");
+        file.setDefault(WORLD_INFO_SEED, "%prefix% <gray>Seed<dark_gray>: <white>" +
+                "<hover:show_text:\"<white>Click to Copy to Clipboard\"><click:copy_to_clipboard:%seed%>%seed%");
         file.setDefault(WORLD_DELETE_DISALLOWED, "%prefix% <red>The world <dark_red>%world% <red>is not deletable");
         file.setDefault(WORLD_DELETE_SUCCEEDED, "%prefix% <white>Successfully deleted the world <green>%world%");
         file.setDefault(WORLD_UNLOAD_FAILED, "%prefix% <red>Failed to unload the world <dark_red>%world%");
@@ -114,7 +115,8 @@ public class Messages {
         file.setDefault(WORLD_INFO_TYPE, "%prefix% <gray>Typ<dark_gray>: <white>%type%");
         file.setDefault(WORLD_INFO_ENVIRONMENT, "%prefix% <gray>Umfeld<dark_gray>: <white>%environment%");
         file.setDefault(WORLD_INFO_GENERATOR, "%prefix% <gray>Generator<dark_gray>: <white>%generator%");
-        file.setDefault(WORLD_INFO_SEED, "%prefix% <gray>Startwert<dark_gray>: <white>%seed%");
+        file.setDefault(WORLD_INFO_SEED, "%prefix% <gray>Startwert<dark_gray>: <white>" +
+                "<hover:show_text:\"<white>Klicken, um in die Zwischenablage zu kopieren\"><click:copy_to_clipboard:%seed%>%seed%");
         file.setDefault(WORLD_DELETE_DISALLOWED, "%prefix% <red>Die Welt <dark_red>%world% <red>ist nicht löschbar");
         file.setDefault(WORLD_DELETE_SUCCEEDED, "%prefix% <white>Die Welt <green>%world%<white> wurde erfolgreich gelöscht");
         file.setDefault(WORLD_UNLOAD_FAILED, "%prefix% <red>Die Welt <green>%world%<red> konnte nicht entladen werden");
