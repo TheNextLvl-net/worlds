d6ebce81b6ebb3980e48b8d9150008b6f68481b6
NonSwag <mrminecraft00@gmail.com>
2023-05-14 21:43:15 +0200

    switched uuid to string id

--- api/src/main/java/net/thenextlvl/worlds/link/Link.java
+++ api/src/main/java/net/thenextlvl/worlds/link/Link.java
@@ -1,6 +1,5 @@
 package net.thenextlvl.worlds.link;
 
-import com.google.gson.GsonBuilder;
 import core.api.file.FileIO;
 import core.api.file.format.GsonFile;
 import lombok.AccessLevel;
@@ -15,21 +14,16 @@ import java.util.stream.Stream;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class Link {
-    private static final HashMap<UUID, Link> links = new HashMap<>();
+    private static final Map<String, Link> links = new HashMap<>();
     private final GsonFile<WorldLink> file;
-    private final UUID uniqueId;
+    private final String id;
 
-    private Link(WorldLink link, UUID uuid) {
-        this(new GsonFile<>(new File(Bukkit.getWorldContainer(), uuid + ".link"), link) {
-            @Override
-            public GsonBuilder load(GsonBuilder builder) {
-                return new GsonBuilder().setPrettyPrinting();
-            }
-        }, uuid);
+    private Link(WorldLink link, String id) {
+        this(new GsonFile<>(new File(Bukkit.getWorldContainer(), id + ".link"), link), id);
     }
 
-    private Link register() {
-        links.put(getUniqueId(), this);
+    public Link register() {
+        links.put(id, this);
         return this;
     }
 
@@ -38,9 +32,14 @@ public class Link {
         return this;
     }
 
-    public static Link getOrCreate(UUID uuid) {
-        if (!links.containsKey(uuid)) new Link(WorldLink.empty(), uuid);
-        return links.get(uuid);
+    public static Link getOrCreate(String id) {
+        if (links.containsKey(id)) return links.get(id);
+        return new Link(WorldLink.empty(), id).register();
+    }
+
+    public static Link of(File file) {
+        var id = file.getName().substring(0, file.getName().length() - 5);
+        return new Link(WorldLink.of(file, WorldLink.empty()), id);
     }
 
     public static Stream<WorldLink> links() {
