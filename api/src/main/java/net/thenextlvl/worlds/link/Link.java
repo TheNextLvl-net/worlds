176a2832ffaa1b62fd02339bd8f3cad0d42249f1
NonSwag <mrminecraft00@gmail.com>
2023-05-17 22:24:07 +0200

    replaced world link with simpler link api

--- api/src/main/java/net/thenextlvl/worlds/link/Link.java
+++ api/src/main/java/net/thenextlvl/worlds/link/Link.java
@@ -1,60 +1,15 @@
 package net.thenextlvl.worlds.link;
 
-import core.api.file.FileIO;
-import core.api.file.format.GsonFile;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.bukkit.Bukkit;
+import com.google.gson.annotations.SerializedName;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-import java.util.*;
-import java.util.stream.Stream;
-
-@Getter
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-public class Link {
-    private static final Map<String, Link> links = new HashMap<>();
-    private final GsonFile<WorldLink> file;
-    private final String id;
-
-    private Link(WorldLink link, String id) {
-        this(new GsonFile<>(new File(Bukkit.getWorldContainer(), id + ".link"), link), id);
-    }
-
-    public Link register() {
-        links.put(id, this);
-        return this;
-    }
-
-    public Link save() {
-        file.save();
-        return this;
-    }
-
-    public static Link getOrCreate(String id) {
-        if (links.containsKey(id)) return links.get(id);
-        return new Link(WorldLink.empty(), id).register();
-    }
-
-    public static Link of(File file) {
-        var id = file.getName().substring(0, file.getName().length() - 5);
-        return new Link(WorldLink.of(file, WorldLink.empty()), id);
-    }
-
-    public static Stream<WorldLink> links() {
-        return links.values().stream().map(Link::getFile).map(FileIO::getRoot);
-    }
-
-    public static List<File> findLinkFiles() {
-        var files = Bukkit.getWorldContainer().listFiles(file ->
-                file.isFile() && file.getName().endsWith(".link"));
-        return files != null ? Arrays.asList(files) : Collections.emptyList();
-    }
-
-    public static List<WorldLink> findLinks() {
-        return findLinkFiles().stream()
-                .map(WorldLink::of)
-                .toList();
-    }
+public record Link(
+        @SerializedName("portal")
+        @NotNull PortalType portalType,
+        @SerializedName("source")
+        @Nullable String first,
+        @SerializedName("destination")
+        @Nullable String second
+) {
 }
