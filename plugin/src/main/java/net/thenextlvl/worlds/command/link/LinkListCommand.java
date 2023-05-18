83fb756d8d3bceda17e292a4303883bb486cd469
NonSwag <mrminecraft00@gmail.com>
2023-05-18 12:19:11 +0200

    finished link list command

--- plugin/src/main/java/net/thenextlvl/worlds/command/link/LinkListCommand.java
+++ plugin/src/main/java/net/thenextlvl/worlds/command/link/LinkListCommand.java
@@ -4,6 +4,7 @@ import cloud.commandframework.Command;
 import cloud.commandframework.context.CommandContext;
 import core.api.placeholder.Placeholder;
 import net.thenextlvl.worlds.Worlds;
+import net.thenextlvl.worlds.link.Link;
 import net.thenextlvl.worlds.util.Messages;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -17,11 +18,11 @@ class LinkListCommand {
     }
 
     private static void execute(CommandContext<CommandSender> context) {
-        var links = plugin.linkFile().links().keySet();
+        var links = plugin.linkFile().links().stream().map(Link::toString).toList();
         var sender = context.getSender();
         var locale = sender instanceof Player player ? player.locale() : Messages.ENGLISH;
-        if (links.isEmpty()) sender.sendRichMessage(Messages.WORLD_NO_LINKS.message(locale, sender));
-        else sender.sendRichMessage(Messages.WORLD_LINK_LIST.message(locale, sender,
+        if (links.isEmpty()) sender.sendRichMessage(Messages.LINK_LIST_EMPTY.message(locale, sender));
+        else sender.sendRichMessage(Messages.LINK_LIST.message(locale, sender,
                 Placeholder.of("links", String.join(", ", links)),
                 Placeholder.of("amount", links.size())));
     }
