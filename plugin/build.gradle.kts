e650e44b18dac80ca94403c8f199a76a81f12022
david <mrminecraft00@gmail.com>
2024-07-30 18:54:05 +0200

    Update Java version configuration to use toolchain options

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -11,8 +11,11 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+tasks.compileJava {
+    options.release.set(21)
 }
 
 group = project(":api").group
