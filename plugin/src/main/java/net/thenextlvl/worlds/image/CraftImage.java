5e7750b382d2eb67b5a4dbb1016760bf739dd9b1
david <mrminecraft00@gmail.com>
2024-08-03 00:08:28 +0200

    Refactor CraftImage by removing redundant methods and dependencies

--- plugin/src/main/java/net/thenextlvl/worlds/image/CraftImage.java
+++ plugin/src/main/java/net/thenextlvl/worlds/image/CraftImage.java
@@ -1,106 +1,55 @@
 package net.thenextlvl.worlds.image;
 
-import core.file.FileIO;
-import core.io.IO;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import net.thenextlvl.worlds.Worlds;
+import net.thenextlvl.worlds.WorldsPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 
 import java.io.File;
-import java.io.IOException;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-class CraftImage implements Image {
-    private final FileIO<WorldImage> file;
-    private final Worlds plugin;
+class CraftImage {
+    private final WorldsPlugin plugin;
     private final World world;
 
-    CraftImage(Worlds plugin, World world, WorldImage image) {
-        this(CraftImageProvider.loadFile(IO.of(Bukkit.getWorldContainer(), image.name() + ".image"), image), plugin, world);
-    }
-
-    CraftImage(Worlds plugin, World world) {
-        this(plugin, world, plugin.imageProvider().of(world));
-    }
-
-    @Override
-    public CraftImage save() {
-        file.save();
-        return this;
-    }
-
-    @Override
-    public WorldImage getWorldImage() {
-        return getFile().getRoot();
-    }
-
-    @Override
     public boolean canUnload() {
-        return !Bukkit.isTickingWorlds() && world.getPlayers().isEmpty();
+        return /*!Bukkit.isTickingWorlds() && */world.getPlayers().isEmpty();
     }
 
-    @Override
-    public boolean canDelete() {
-        return getWorld().getKey().toString().equals("minecraft:overworld");
+    public boolean isDeletable() {
+        return !getWorld().getKey().toString().equals("minecraft:overworld");
     }
 
-    @Override
     public boolean unload() {
         return canUnload() && Bukkit.unloadWorld(world, world.isAutoSave());
     }
 
-    @Override
-    public DeleteResult delete(boolean keepImage, boolean keepWorld, boolean schedule) {
-        return schedule ? scheduleDeletion(keepImage, keepWorld) : deleteNow(keepImage, keepWorld);
+    public DeleteResult delete(boolean schedule) {
+        return schedule ? scheduleDeletion() : deleteNow();
     }
 
-    @Override
-    public DeleteResult deleteNow(boolean keepImage, boolean keepWorld) {
-        if (canDelete()) return DeleteResult.WORLD_DELETE_ILLEGAL;
+    public DeleteResult deleteNow() {
+        if (!isDeletable()) return DeleteResult.EXEMPTED;
 
         var fallback = Bukkit.getWorlds().getFirst().getSpawnLocation();
         getWorld().getPlayers().forEach(player -> player.teleport(fallback));
 
-        try {
-            if (!keepImage && file.getIO().exists() && !file.delete())
-                return DeleteResult.IMAGE_DELETE_FAILED;
-        } catch (IOException e) {
-            return DeleteResult.IMAGE_DELETE_FAILED;
-        }
+        if (!unload()) return DeleteResult.UNLOAD_FAILED;
 
-        if (keepImage && keepWorld)
-            return Bukkit.unloadWorld(world, world.isAutoSave())
-                    ? DeleteResult.WORLD_UNLOADED
-                    : DeleteResult.WORLD_UNLOAD_FAILED;
-
-        if (!Bukkit.unloadWorld(world, world.isAutoSave() && keepWorld))
-            return DeleteResult.WORLD_UNLOAD_FAILED;
-
-        if (!keepWorld && !delete(world.getWorldFolder()))
-            return DeleteResult.WORLD_DELETE_FAILED;
-
-        return DeleteResult.WORLD_DELETED;
+        return delete(world.getWorldFolder())
+                ? DeleteResult.SUCCESS
+                : DeleteResult.FAILED;
     }
 
-    @Override
-    public DeleteResult scheduleDeletion(boolean keepImage, boolean keepWorld) {
-        if (keepWorld && (keepImage || !getFile().getIO().exists()))
-            return DeleteResult.WORLD_DELETE_NOTHING;
-
+    public DeleteResult scheduleDeletion() {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            if (!keepWorld) delete(getWorld().getWorldFolder());
-            if (!keepImage) try {
-                getFile().delete();
-            } catch (IOException e) {
-                plugin.getComponentLogger().error("Failed to delete world {}", getWorld().getName(), e);
-            }
+            if (delete(getWorld().getWorldFolder())) return;
+            plugin.getComponentLogger().error("Failed to delete world {}", getWorld().getName());
         }));
-
-        return DeleteResult.WORLD_DELETE_SCHEDULED;
+        return DeleteResult.SCHEDULED;
     }
 
     private boolean delete(File file) {
