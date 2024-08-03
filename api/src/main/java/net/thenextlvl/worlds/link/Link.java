6304b7f3f24b41572332684c3403978370ea8198
david <mrminecraft00@gmail.com>
2024-08-03 22:56:03 +0200

    Remove unused PortalType field from Link record

--- api/src/main/java/net/thenextlvl/worlds/link/Link.java
+++ api/src/main/java/net/thenextlvl/worlds/link/Link.java
@@ -1,16 +1,14 @@
 package net.thenextlvl.worlds.link;
 
 import com.google.gson.annotations.SerializedName;
-import org.bukkit.PortalType;
 import org.bukkit.World;
 
 public record Link(
-        @SerializedName("portal") PortalType portalType,
         @SerializedName("source") World source,
         @SerializedName("destination") World destination
 ) {
     @Override
     public String toString() {
-        return portalType.name().toLowerCase() + ": " + source.getName() + " -> " + destination.getName();
+        return source.key().asMinimalString() + " -> " + destination.key().asMinimalString();
     }
 }
