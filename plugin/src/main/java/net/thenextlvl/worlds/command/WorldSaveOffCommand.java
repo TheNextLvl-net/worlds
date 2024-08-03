3a9382c79590e1ca4b02a2729b4f1acbebf7f7eb
david <mrminecraft00@gmail.com>
2024-08-03 22:42:14 +0200

    Add WorldSaveOffCommand command to disable saving

--- plugin/src/main/java/net/thenextlvl/worlds/command/WorldSaveOffCommand.java
+++ plugin/src/main/java/net/thenextlvl/worlds/command/WorldSaveOffCommand.java
@@ -4,21 +4,20 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
-import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import lombok.RequiredArgsConstructor;
 import net.thenextlvl.worlds.WorldsPlugin;
 
 @RequiredArgsConstructor
 @SuppressWarnings("UnstableApiUsage")
-class WorldExportCommand {
+class WorldSaveOffCommand {
     private final WorldsPlugin plugin;
 
     ArgumentBuilder<CommandSourceStack, ?> create() {
-        return Commands.literal("export")
-                .requires(source -> source.getSender().hasPermission("worlds.command.export"))
-                .then(Commands.argument("world", ArgumentTypes.world())
-                        .executes(context -> {
-                            return Command.SINGLE_SUCCESS;
-                        }));
+        return Commands.literal("save-off")
+                .requires(source -> source.getSender().hasPermission("worlds.command.save-off"))
+                .executes(context -> {
+                    // todo: save-off
+                    return Command.SINGLE_SUCCESS;
+                });
     }
 }
