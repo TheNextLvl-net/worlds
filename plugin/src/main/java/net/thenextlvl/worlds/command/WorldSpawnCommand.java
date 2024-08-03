4d21c21b6abeeaaea6caf0adc0c9a13200058f49
david <mrminecraft00@gmail.com>
2024-08-03 22:43:37 +0200

    Restrict 'spawn' command to players and implement teleport.

--- plugin/src/main/java/net/thenextlvl/worlds/command/WorldSpawnCommand.java
+++ plugin/src/main/java/net/thenextlvl/worlds/command/WorldSpawnCommand.java
@@ -6,6 +6,9 @@ import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import lombok.RequiredArgsConstructor;
 import net.thenextlvl.worlds.WorldsPlugin;
+import org.bukkit.entity.Player;
+
+import static org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.COMMAND;
 
 @RequiredArgsConstructor
 @SuppressWarnings("UnstableApiUsage")
@@ -14,8 +17,12 @@ class WorldSpawnCommand {
 
     ArgumentBuilder<CommandSourceStack, ?> create() {
         return Commands.literal("spawn")
-                .requires(source -> source.getSender().hasPermission("worlds.command.spawn"))
+                .requires(source -> source.getSender().hasPermission("worlds.command.spawn")
+                                    && source.getSender() instanceof Player)
                 .executes(context -> {
+                    var player = (Player) context.getSource().getSender();
+                    player.teleportAsync(player.getWorld().getSpawnLocation(), COMMAND);
+                    // todo: add message
                     return Command.SINGLE_SUCCESS;
                 });
     }
