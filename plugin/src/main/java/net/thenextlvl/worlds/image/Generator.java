60d530e3f5afc63d0f3e6d0ec0ec036da4ecd94b
david <mrminecraft00@gmail.com>
2024-08-04 13:39:44 +0200

    Remove unused 'of' method from Generator class

--- plugin/src/main/java/net/thenextlvl/worlds/image/Generator.java
+++ plugin/src/main/java/net/thenextlvl/worlds/image/Generator.java
@@ -1,21 +1,9 @@
 package net.thenextlvl.worlds.image;
 
-import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.PluginClassLoader;
 import org.jetbrains.annotations.Nullable;
 
 public record Generator(String plugin, @Nullable String id) {
-
-    @Nullable
-    @SuppressWarnings("UnstableApiUsage")
-    public static Generator of(World world) {
-        if (world.getGenerator() == null) return null;
-        var loader = world.getGenerator().getClass().getClassLoader();
-        if (!(loader instanceof PluginClassLoader pluginLoader)) return null;
-        if (pluginLoader.getPlugin() == null) return null;
-        return new Generator(pluginLoader.getPlugin().getName(), null);
-    }
 
     public static boolean hasChunkGenerator(Class<? extends Plugin> clazz) {
         try {
