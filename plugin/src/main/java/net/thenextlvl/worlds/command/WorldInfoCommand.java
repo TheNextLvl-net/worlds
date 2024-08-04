514795b712581a034ed1f5e43772aa842bd62223
david <mrminecraft00@gmail.com>
2024-08-04 13:39:32 +0200

    Add detailed world info command to display world data

--- plugin/src/main/java/net/thenextlvl/worlds/command/WorldInfoCommand.java
+++ plugin/src/main/java/net/thenextlvl/worlds/command/WorldInfoCommand.java
@@ -2,12 +2,20 @@ package net.thenextlvl.worlds.command;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.ArgumentBuilder;
+import core.nbt.tag.CompoundTag;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
+import io.papermc.paper.plugin.provider.classloader.ConfiguredPluginClassLoader;
 import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.thenextlvl.worlds.WorldsPlugin;
 import net.thenextlvl.worlds.command.suggestion.WorldSuggestionProvider;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @SuppressWarnings("UnstableApiUsage")
@@ -20,7 +28,49 @@ class WorldInfoCommand {
                 .then(Commands.argument("world", ArgumentTypes.world())
                         .suggests(new WorldSuggestionProvider<>(plugin))
                         .executes(context -> {
-                            return Command.SINGLE_SUCCESS;
-                        }));
+                            var world = context.getArgument("world", World.class);
+                            return list(context.getSource().getSender(), world);
+                        }))
+                .executes(context -> {
+                    if (!(context.getSource().getSender() instanceof Player player)) {
+                        plugin.bundle().sendMessage(context.getSource().getSender(), "command.sender");
+                        return 0;
+                    } else return list(context.getSource().getSender(), player.getWorld());
+                });
+    }
+
+    private int list(CommandSender sender, World world) {
+        var root = plugin.levelView().getLevelDataFile(world.getWorldFolder()).getRoot();
+        var data = root.<CompoundTag>optional("Data");
+        var settings = data.flatMap(tag -> tag.<CompoundTag>optional("WorldGenSettings"));
+        var dimensions = settings.flatMap(tag -> tag.<CompoundTag>optional("dimensions"));
+        var dimension = dimensions.flatMap(tag -> tag.<CompoundTag>optional(
+                plugin.levelView().getDimension(tag, world.getEnvironment())));
+        var generator = dimension.flatMap(tag -> tag.<CompoundTag>optional("generator"));
+
+        var environment = dimensions.map(tag -> plugin.levelView().getDimension(tag, world.getEnvironment()));
+        var type = generator.flatMap(plugin.levelView()::getGeneratorType);
+
+        plugin.bundle().sendMessage(sender, "world.info.name",
+                Placeholder.parsed("world", world.key().asString()));
+        plugin.bundle().sendMessage(sender, "world.info.players",
+                Placeholder.parsed("players", String.valueOf(world.getPlayers().size())));
+        plugin.bundle().sendMessage(sender, "world.info.type",
+                Placeholder.parsed("type", type.orElse("unknown")));
+        plugin.bundle().sendMessage(sender, "world.info.dimension",
+                Placeholder.parsed("dimension", environment.orElse("unknown")));
+        getGenerator(world).ifPresent(gen -> plugin.bundle().sendMessage(sender,
+                "world.info.generator", Placeholder.parsed("generator", gen)));
+        plugin.bundle().sendMessage(sender, "world.info.seed",
+                Placeholder.parsed("seed", String.valueOf(world.getSeed())));
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private Optional<String> getGenerator(World world) {
+        if (world.getGenerator() == null) return Optional.empty();
+        var loader = world.getGenerator().getClass().getClassLoader();
+        if (!(loader instanceof ConfiguredPluginClassLoader pluginLoader)) return Optional.empty();
+        if (pluginLoader.getPlugin() == null) return Optional.empty();
+        return Optional.of(pluginLoader.getPlugin().getName());
     }
 }
