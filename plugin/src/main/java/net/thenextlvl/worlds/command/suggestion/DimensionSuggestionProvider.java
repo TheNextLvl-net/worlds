9da7c2af7ffec5c509ce956aaa290b81bd536072
david <mrminecraft00@gmail.com>
2024-08-05 21:01:19 +0200

    refactor dimension suggestion provider

--- plugin/src/main/java/net/thenextlvl/worlds/command/suggestion/DimensionSuggestionProvider.java
+++ plugin/src/main/java/net/thenextlvl/worlds/command/suggestion/DimensionSuggestionProvider.java
@@ -1,15 +1,16 @@
 package net.thenextlvl.worlds.command.suggestion;
 
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.suggestion.SuggestionProvider;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import core.paper.command.SuggestionProvider;
 
 import java.util.concurrent.CompletableFuture;
 
-public class DimensionSuggestionProvider<I> implements SuggestionProvider<I> {
+public class DimensionSuggestionProvider implements SuggestionProvider {
+
     @Override
-    public CompletableFuture<Suggestions> getSuggestions(CommandContext<I> context, SuggestionsBuilder builder) {
+    public CompletableFuture<Suggestions> suggest(CommandContext<?> context, SuggestionsBuilder builder) {
         builder.suggest("minecraft:overworld");
         builder.suggest("minecraft:the_end");
         builder.suggest("minecraft:the_nether");
