887093f803f2997ee2886b9dd05c917f16686ea3
david <mrminecraft00@gmail.com>
2024-08-08 20:44:04 +0200

    Refactor DimensionArgument and add plugin dependency
    Refactor `DimensionArgument` to throw an exception for unsupported custom dimensions and require `WorldsPlugin` as a constructor parameter. Update `DimensionSuggestionProvider` to support dynamic suggestions using a map and inject the `WorldsPlugin` dependency.

--- plugin/src/main/java/net/thenextlvl/worlds/command/argument/DimensionArgument.java
+++ plugin/src/main/java/net/thenextlvl/worlds/command/argument/DimensionArgument.java
@@ -3,19 +3,18 @@ package net.thenextlvl.worlds.command.argument;
 import core.paper.command.WrappedArgumentType;
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import net.kyori.adventure.key.Key;
+import net.thenextlvl.worlds.WorldsPlugin;
 import net.thenextlvl.worlds.command.suggestion.DimensionSuggestionProvider;
 import org.bukkit.World;
 
 @SuppressWarnings("UnstableApiUsage")
 public class DimensionArgument extends WrappedArgumentType<Key, World.Environment> {
-    public DimensionArgument() {
-        super(ArgumentTypes.key(), (reader, type) -> {
-            return switch (type.asString()) {
-                case "minecraft:overworld" -> World.Environment.NORMAL;
-                case "minecraft:the_end" -> World.Environment.THE_END;
-                case "minecraft:the_nether" -> World.Environment.NETHER;
-                default -> World.Environment.CUSTOM;
-            };
-        }, new DimensionSuggestionProvider());
+    public DimensionArgument(WorldsPlugin plugin) {
+        super(ArgumentTypes.key(), (reader, type) -> switch (type.asString()) {
+            case "minecraft:overworld" -> World.Environment.NORMAL;
+            case "minecraft:the_end" -> World.Environment.THE_END;
+            case "minecraft:the_nether" -> World.Environment.NETHER;
+            default -> throw new IllegalArgumentException("Custom dimensions are not yet supported");
+        }, new DimensionSuggestionProvider(plugin));
     }
 }
--- plugin/src/main/java/net/thenextlvl/worlds/command/suggestion/DimensionSuggestionProvider.java
+++ plugin/src/main/java/net/thenextlvl/worlds/command/suggestion/DimensionSuggestionProvider.java
@@ -4,16 +4,31 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import core.paper.command.SuggestionProvider;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.worlds.WorldsPlugin;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+@RequiredArgsConstructor
+@SuppressWarnings("UnstableApiUsage")
 public class DimensionSuggestionProvider implements SuggestionProvider {
+    private final WorldsPlugin plugin;
+
+    private final Map<String, String> dimensions = Map.of(
+            "minecraft:overworld", "environment.normal",
+            "minecraft:the_end", "environment.end",
+            "minecraft:the_nether", "environment.nether"
+    );
 
     @Override
     public CompletableFuture<Suggestions> suggest(CommandContext<?> context, SuggestionsBuilder builder) {
-        builder.suggest("minecraft:overworld");
-        builder.suggest("minecraft:the_end");
-        builder.suggest("minecraft:the_nether");
+        var sender = ((CommandSourceStack) context.getSource()).getSender();
+        dimensions.entrySet().stream()
+                .filter(entry -> entry.getKey().contains(builder.getRemaining()))
+                .forEach(entry -> builder.suggest(entry.getKey(), () ->
+                        plugin.bundle().format(sender, entry.getValue())));
         return builder.buildFuture();
     }
 }
