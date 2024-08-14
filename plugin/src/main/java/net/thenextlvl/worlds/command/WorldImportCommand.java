67651a6c07939764791f8c5517a819712ba6e4eb
david <mrminecraft00@gmail.com>
2024-08-14 21:28:19 +0200

    Persist world data after import
    Add logic to persist and save level data when a world is successfully imported. This ensures that the world's state is maintained and properly recorded.

--- plugin/src/main/java/net/thenextlvl/worlds/command/WorldImportCommand.java
+++ plugin/src/main/java/net/thenextlvl/worlds/command/WorldImportCommand.java
@@ -53,12 +53,20 @@ class WorldImportCommand {
         var world = plugin.levelView().isLevel(level) ? environment != null
                 ? plugin.levelView().loadLevel(level, environment, Optional::isEmpty)
                 : plugin.levelView().loadLevel(level, Optional::isEmpty) : null;
+
         var message = world != null ? "world.import.success" : "world.import.failed";
         plugin.bundle().sendMessage(context.getSource().getSender(), message,
                 Placeholder.parsed("world", world != null ? world.key().asString()
                         : key != null ? key.asString() : name));
+
         if (world != null && context.getSource().getSender() instanceof Entity entity)
             entity.teleportAsync(world.getSpawnLocation(), COMMAND);
+
+        if (world != null) {
+            plugin.persistWorld(world, true);
+            plugin.levelView().saveLevelData(world, true);
+        }
+
         return world != null ? Command.SINGLE_SUCCESS : 0;
     }
 }
