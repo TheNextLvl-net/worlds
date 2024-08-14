2d638b7cdbab8d1419b5d12f949780cdc70b5eba
david <mrminecraft00@gmail.com>
2024-08-14 18:12:03 +0200

    Add filtering to command argument and suggestion providers
    Update `RelativeArgument` and `RelativeSuggestionProvider` to include filtering predicates. Extend `WorldSuggestionProvider` with a filter for more controlled suggestions.

--- plugin/src/main/java/net/thenextlvl/worlds/command/argument/RelativeArgument.java
+++ plugin/src/main/java/net/thenextlvl/worlds/command/argument/RelativeArgument.java
@@ -6,11 +6,13 @@ import net.kyori.adventure.key.Key;
 import net.thenextlvl.worlds.command.suggestion.RelativeSuggestionProvider;
 import net.thenextlvl.worlds.link.Relative;
 
+import java.util.function.Predicate;
+
 @SuppressWarnings("UnstableApiUsage")
 public class RelativeArgument extends WrappedArgumentType<Key, Relative> {
-    public RelativeArgument() {
+    public RelativeArgument(Predicate<Relative> filter) {
         super(ArgumentTypes.key(), (reader, type) -> Relative.valueOf(type).orElseThrow(() ->
                         new IllegalArgumentException("Unknown relative: " + type.asString())),
-                new RelativeSuggestionProvider());
+                new RelativeSuggestionProvider(filter));
     }
 }
--- plugin/src/main/java/net/thenextlvl/worlds/command/suggestion/RelativeSuggestionProvider.java
+++ plugin/src/main/java/net/thenextlvl/worlds/command/suggestion/RelativeSuggestionProvider.java
@@ -4,15 +4,21 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import core.paper.command.SuggestionProvider;
+import lombok.RequiredArgsConstructor;
 import net.thenextlvl.worlds.link.Relative;
 
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Predicate;
 
+@RequiredArgsConstructor
 public class RelativeSuggestionProvider implements SuggestionProvider {
+    private final Predicate<Relative> filter;
+
     @Override
     public CompletableFuture<Suggestions> suggest(CommandContext<?> context, SuggestionsBuilder builder) {
         Arrays.stream(Relative.values())
+                .filter(filter)
                 .map(relative -> relative.key().asString())
                 .filter(s -> s.contains(builder.getRemaining()))
                 .forEach(builder::suggest);
