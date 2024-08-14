8ca3f180fa91bee7f41e856df278659f1b4c58b4
david <mrminecraft00@gmail.com>
2024-08-14 17:39:52 +0200

    Switch to NamespacedKey and add convenience methods.
    Replaced Key with NamespacedKey for relative world identifiers. Added two static methods to fetch Relative enum instances by Key or World.Environment for improved usability.

--- api/src/main/java/net/thenextlvl/worlds/link/Relative.java
+++ api/src/main/java/net/thenextlvl/worlds/link/Relative.java
@@ -5,14 +5,34 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.key.Keyed;
+import org.bukkit.NamespacedKey;
+import org.bukkit.World;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 @Getter
 @RequiredArgsConstructor
 @Accessors(fluent = true)
 public enum Relative implements Keyed {
-    OVERWORLD(Key.key("relative", "overworld")),
-    NETHER(Key.key("relative", "nether")),
-    THE_END(Key.key("relative", "the_end"));
+    OVERWORLD(new NamespacedKey("relative", "overworld")),
+    NETHER(new NamespacedKey("relative", "nether")),
+    THE_END(new NamespacedKey("relative", "the_end"));
 
-    private final Key key;
+    private final NamespacedKey key;
+
+    public static Optional<Relative> valueOf(Key key) {
+        return Arrays.stream(values())
+                .filter(value -> value.key().equals(key))
+                .findAny();
+    }
+
+    public static Optional<Relative> valueOf(World.Environment environment) {
+        return Optional.ofNullable(switch (environment) {
+            case NORMAL -> Relative.OVERWORLD;
+            case NETHER -> Relative.NETHER;
+            case THE_END -> Relative.THE_END;
+            default -> null;
+        });
+    }
 }
