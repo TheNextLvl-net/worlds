118e284ebe290e15bbda7968369d29fd5affd87b
david <mrminecraft00@gmail.com>
2024-08-15 17:56:29 +0200

    Switch to WorldLoadEvent listener
    Replaced ServerLoadEvent with WorldLoadEvent for better specificity. Enhanced logging to provide clearer information on dimension loads and error handling.

--- plugin/src/main/java/net/thenextlvl/worlds/listener/ServerListener.java
+++ plugin/src/main/java/net/thenextlvl/worlds/listener/ServerListener.java
@@ -5,22 +5,28 @@ import net.thenextlvl.worlds.WorldsPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.server.ServerLoadEvent;
+import org.bukkit.event.world.WorldLoadEvent;
 
 @RequiredArgsConstructor
 public class ServerListener implements Listener {
     private final WorldsPlugin plugin;
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onServerLoad(ServerLoadEvent event) {
-        if (!event.getType().equals(ServerLoadEvent.LoadType.STARTUP)) return;
+    public void onWorldLoad(WorldLoadEvent event) {
+        if (!event.getWorld().key().asString().equals("minecraft:overworld")) return;
         plugin.levelView().listLevels()
                 .filter(plugin.levelView()::canLoad)
                 .forEach(level -> {
                     try {
                         var world = plugin.levelView().loadLevel(level);
+                        if (world != null) plugin.getComponentLogger().debug("Loaded dimension {} at {}",
+                                world.key().asString(), level.getPath());
+                        else plugin.getComponentLogger().error("Failed to load the level {}", level.getPath());
                     } catch (Exception e) {
-                        plugin.getComponentLogger().error("Could not load level {}", level.getPath(), e);
+                        plugin.getComponentLogger().error("An unexpected error occurred while loading the level {}",
+                                level.getPath(), e);
+                        plugin.getComponentLogger().error("Please report the error above on GitHub: {}",
+                                "https://github.com/TheNextLvl-net/worlds/issues/new/choose");
                     }
                 });
     }
