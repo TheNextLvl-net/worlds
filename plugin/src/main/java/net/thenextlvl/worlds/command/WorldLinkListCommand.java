d88f8a2ed6c8c13e6dbbda2af50043f877211a23
david <mrminecraft00@gmail.com>
2024-08-15 16:12:22 +0200

    Refactor world link listing command
    Update WorldLinkListCommand to include detailed world link information. Filter worlds by environment and generate a list of links using the Relative enum. Send appropriate messages to the command sender based on the results.

--- plugin/src/main/java/net/thenextlvl/worlds/command/WorldLinkListCommand.java
+++ plugin/src/main/java/net/thenextlvl/worlds/command/WorldLinkListCommand.java
@@ -5,12 +5,14 @@ import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
-import io.papermc.paper.event.player.PlayerBedFailEnterEvent;
 import lombok.RequiredArgsConstructor;
-import net.minecraft.server.level.PlayerRespawnLogic;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.thenextlvl.worlds.WorldsPlugin;
-import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerRespawnEvent;
+import net.thenextlvl.worlds.link.Relative;
+import org.bukkit.World;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @SuppressWarnings("UnstableApiUsage")
@@ -24,6 +26,19 @@ public class WorldLinkListCommand {
     }
 
     private int list(CommandContext<CommandSourceStack> context) {
-        return Command.SINGLE_SUCCESS;
+        var sender = context.getSource().getSender();
+        var links = plugin.getServer().getWorlds().stream()
+                .filter(world -> world.getEnvironment().equals(World.Environment.NORMAL))
+                .<String>mapMulti((world, consumer) -> Arrays.stream(Relative.values())
+                        .filter(relative -> !relative.equals(Relative.OVERWORLD))
+                        .map(relative -> plugin.linkController().getTarget(world, relative).orElse(null))
+                        .filter(Objects::nonNull)
+                        .forEach(key -> consumer.accept(world.key().asString() + " <-> " + key.asString())))
+                .toList();
+        if (links.isEmpty()) plugin.bundle().sendMessage(sender, "world.link.list.empty");
+        else plugin.bundle().sendMessage(sender, "world.link.list",
+                Placeholder.parsed("links", String.join(",<newline>", links)),
+                Placeholder.parsed("amount", String.valueOf(links.size())));
+        return links.isEmpty() ? 0 : Command.SINGLE_SUCCESS;
     }
 }
