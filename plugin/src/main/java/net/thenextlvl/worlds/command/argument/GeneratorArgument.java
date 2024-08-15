83fffd228377a2543b8c506831c10b2474415ec2
david <mrminecraft00@gmail.com>
2024-08-15 16:45:43 +0200

    Refactor exception handling in GeneratorArgument
    Replaced basic exception throws with Preconditions checks for improved readability and clarity. This change ensures that null and state conditions for the generator plugin are explicitly checked before proceeding.

--- plugin/src/main/java/net/thenextlvl/worlds/command/argument/GeneratorArgument.java
+++ plugin/src/main/java/net/thenextlvl/worlds/command/argument/GeneratorArgument.java
@@ -1,5 +1,6 @@
 package net.thenextlvl.worlds.command.argument;
 
+import com.google.common.base.Preconditions;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import core.paper.command.WrappedArgumentType;
 import net.thenextlvl.worlds.WorldsPlugin;
@@ -12,8 +13,8 @@ public class GeneratorArgument extends WrappedArgumentType<String, GeneratorArgu
         super(StringArgumentType.string(), (reader, type) -> {
             var split = type.split(":", 2);
             var generator = plugin.getServer().getPluginManager().getPlugin(split[0]);
-            if (generator == null) throw new IllegalArgumentException("Unknown plugin");
-            if (!generator.isEnabled()) throw new IllegalStateException("Plugin is not enabled");
+            Preconditions.checkNotNull(generator, "Unknown plugin");
+            Preconditions.checkState(generator.isEnabled(), "Plugin is not enabled");
             return new Generator(generator, split.length > 1 ? split[1] : null);
         }, new GeneratorSuggestionProvider(plugin));
     }
