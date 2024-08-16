8d8412a17cdb14e8f415414adad34185865cf8fb
david <mrminecraft00@gmail.com>
2024-08-16 18:28:06 +0200

    Add GeneratorException for handling plugin generator issues
    Introduced a custom exception, `GeneratorException`, to handle scenarios where a requested plugin cannot be found, is disabled, or does not provide the required chunk generator or biome provider. Updated package-info.java to enforce not null constraints across the package.

---
+++ api/src/main/java/net/thenextlvl/worlds/api/exception/package-info.java
@@ -0,0 +1,10 @@
+@TypesAreNotNullByDefault
+@FieldsAreNotNullByDefault
+@ParametersAreNotNullByDefault
+@MethodsReturnNotNullByDefault
+package net.thenextlvl.worlds.api.exception;
+
+import core.annotation.FieldsAreNotNullByDefault;
+import core.annotation.MethodsReturnNotNullByDefault;
+import core.annotation.ParametersAreNotNullByDefault;
+import core.annotation.TypesAreNotNullByDefault;
