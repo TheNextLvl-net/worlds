ef88ac3c3b40d1a61e134ca97cd23c9bc18bc17f
david <mrminecraft00@gmail.com>
2024-08-17 03:49:36 +0200

    Add levelBuilder method to WorldsProvider interface
    Introduced a new method, levelBuilder, to the WorldsProvider interface, which takes a File parameter. This addition enhances the functionality of WorldsProvider by allowing the creation of levels through LevelBuilder.

--- api/src/main/java/net/thenextlvl/worlds/api/WorldsProvider.java
+++ api/src/main/java/net/thenextlvl/worlds/api/WorldsProvider.java
@@ -1,12 +1,17 @@
 package net.thenextlvl.worlds.api;
 
 import net.thenextlvl.worlds.api.link.LinkController;
+import net.thenextlvl.worlds.api.model.LevelBuilder;
 import net.thenextlvl.worlds.api.view.GeneratorView;
 import net.thenextlvl.worlds.api.view.LevelView;
 import org.bukkit.plugin.Plugin;
 
+import java.io.File;
+
 public interface WorldsProvider extends Plugin {
     GeneratorView generatorView();
+
+    LevelBuilder levelBuilder(File level);
 
     LevelView levelView();
 
