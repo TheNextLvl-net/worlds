ed6bce204d09fc3f9cfb895d2ba647911a97c39e
david <github@nonswag.dev>
2025-03-18 21:18:56 +0100

    Update Shadow plugin to version 9.0.0-beta9
    Replaced the outdated Shadow plugin identifier and updated it to the latest beta version for compatibility and improved features. This ensures the build script stays up-to-date with current standards.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("io.papermc.hangar-publish-plugin") version "0.1.2"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
     id("net.minecrell.plugin-yml.paper") version "0.6.0"
-    id("io.github.goooler.shadow") version "8.1.8"
+    id("com.gradleup.shadow") version "9.0.0-beta9"
     id("com.modrinth.minotaur") version "2.+"
 }
 
