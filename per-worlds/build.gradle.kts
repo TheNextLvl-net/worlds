e5e74510536d6504e43de8a57010a55f53e39bc6
david <github@nonswag.dev>
2025-03-18 21:51:32 +0100

    Rename and enhance build script for PerWorlds module
    Renamed `per-world` to `per-worlds` and added new dependencies, plugin metadata, and publication configuration. Introduced Hangar publishing settings and additional API dependencies to support enhanced functionality. Updated the plugin descriptor for improved modular and publishing support.

---
+++ per-worlds/build.gradle.kts
@@ -0,0 +1,72 @@
+import io.papermc.hangarpublishplugin.model.Platforms
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+import net.minecrell.pluginyml.paper.PaperPluginDescription
+
+plugins {
+    id("java")
+    id("java-library")
+    id("com.gradleup.shadow") version "9.0.0-beta9"
+    id("io.papermc.hangar-publish-plugin") version "0.1.2"
+    id("net.minecrell.plugin-yml.paper") version "0.6.0"
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+tasks.compileJava {
+    options.release.set(21)
+}
+
+group = rootProject.group
+version = rootProject.version
+
+repositories {
+    mavenCentral()
+    maven("https://repo.thenextlvl.net/releases")
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnlyApi(project(":api"))
+
+    api("org.bstats:bstats-bukkit:3.1.0")
+}
+
+paper {
+    name = "PerWorlds"
+    main = "net.thenextlvl.perworlds.WorldsPlugin"
+    apiVersion = "1.21"
+    description = "Per-world customization for gameplay and settings"
+    load = BukkitPluginDescription.PluginLoadOrder.POSTWORLD
+    website = "https://thenextlvl.net"
+    authors = listOf("NonSwag")
+    foliaSupported = true
+    serverDependencies {
+        register("Worlds") {
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+            required = false
+        }
+    }
+}
+
+val versionString: String = project.version as String
+val isRelease: Boolean = !versionString.contains("-pre")
+
+val versions: List<String> = (property("gameVersions") as String)
+    .split(",")
+    .map { it.trim() }
+
+hangarPublish { // docs - https://docs.papermc.io/misc/hangar-publishing
+    publications.register("per-worlds") {
+        id.set("PerWorlds")
+        version.set(versionString)
+        channel.set(if (isRelease) "Release" else "Snapshot")
+        apiKey.set(System.getenv("HANGAR_API_TOKEN"))
+        platforms.register(Platforms.PAPER) {
+            jar.set(tasks.shadowJar.flatMap { it.archiveFile })
+            platformVersions.set(versions)
+        }
+    }
+}
