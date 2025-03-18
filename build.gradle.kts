6bde080a6822d086d9ad3dbcc66faf15c407bcdf
david <github@nonswag.dev>
2025-03-18 21:19:12 +0100

    Refactor plugin order in build.gradle.kts
    Reordered Gradle plugins for better organization and clarity. This change has no functional impact but improves readability and maintainability of the build script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 plugins {
     id("java")
+    id("com.gradleup.shadow") version "9.0.0-beta9"
+    id("com.modrinth.minotaur") version "2.+"
     id("io.papermc.hangar-publish-plugin") version "0.1.2"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
     id("net.minecrell.plugin-yml.paper") version "0.6.0"
-    id("com.gradleup.shadow") version "9.0.0-beta9"
-    id("com.modrinth.minotaur") version "2.+"
 }
 
 java {
