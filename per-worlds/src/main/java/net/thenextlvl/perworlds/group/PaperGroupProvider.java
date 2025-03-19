a585d4855fe21f1c89d6beb24bc1ffe635a41719
david <github@nonswag.dev>
2025-03-19 18:49:39 +0100

    Add PaperGroupProvider implementation for group management
    This class implements the GroupProvider interface, enabling group management for worlds in a Paper-based environment. It provides functionality for creating, retrieving, and modifying groups, ensuring efficient handling of world groups in the system.

---
+++ per-worlds/src/main/java/net/thenextlvl/perworlds/group/PaperGroupProvider.java
@@ -0,0 +1,69 @@
+package net.thenextlvl.perworlds.group;
+
+import com.google.common.base.Preconditions;
+import net.kyori.adventure.key.Key;
+import net.thenextlvl.perworlds.GroupProvider;
+import net.thenextlvl.perworlds.WorldGroup;
+import org.bukkit.World;
+import org.jetbrains.annotations.Unmodifiable;
+import org.jspecify.annotations.NullMarked;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@NullMarked
+public class PaperGroupProvider implements GroupProvider {
+    private final List<WorldGroup> groups = new ArrayList<>();
+
+    @Override
+    public @Unmodifiable List<WorldGroup> getGroups() {
+        return List.copyOf(groups);
+    }
+
+    @Override
+    public Optional<WorldGroup> getGroup(Key key) {
+        return groups.stream().filter(group -> group.key().equals(key)).findAny();
+    }
+
+    @Override
+    public Optional<WorldGroup> getGroup(World world) {
+        return groups.stream().filter(group -> group.containsWorld(world)).findAny();
+    }
+
+    @Override
+    public WorldGroup.Builder createGroup(Key key) {
+        return new PaperWorldGroup.Builder(key);
+    }
+
+    @Override
+    public boolean addGroup(WorldGroup group) {
+        Preconditions.checkState(!hasGroup(group), "Group is already registered");
+        return groups.add(group);
+    }
+
+    @Override
+    public boolean hasGroup(Key key) {
+        return groups.stream().anyMatch(group -> group.key().equals(key));
+    }
+
+    @Override
+    public boolean hasGroup(World world) {
+        return groups.stream().anyMatch(group -> group.containsWorld(world));
+    }
+
+    @Override
+    public boolean hasGroup(WorldGroup group) {
+        return groups.contains(group);
+    }
+
+    @Override
+    public boolean removeGroup(Key key) {
+        return groups.removeIf(group -> group.key().equals(key));
+    }
+
+    @Override
+    public boolean removeGroup(WorldGroup group) {
+        return groups.remove(group);
+    }
+}
