a693a45acdbe76cfe937ea33cd4317f5007b3476
david <github@nonswag.dev>
2025-03-19 18:22:41 +0100

    Refactor GroupProvider to an interface and add new methods
    Converted GroupProvider from a class to an interface to improve flexibility and extensibility. Added methods to retrieve groups, as well as specific group fetching using a Key or World. This enhances the API's usability and aligns with best practices.

--- per-worlds-api/src/main/java/net/thenextlvl/perworlds/GroupProvider.java
+++ per-worlds-api/src/main/java/net/thenextlvl/perworlds/GroupProvider.java
@@ -1,4 +1,15 @@
 package net.thenextlvl.perworlds;
 
-public class GroupProvider {
+import net.kyori.adventure.key.Key;
+import org.bukkit.World;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GroupProvider {
+    List<WorldGroup> getGroups();
+
+    Optional<WorldGroup> getGroup(Key key);
+
+    Optional<WorldGroup> getGroup(World world);
 }
