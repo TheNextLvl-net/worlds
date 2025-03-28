6db6bfe19e27b85813f3ad4d27c8b88a246ca9c4
david <github@nonswag.dev>
2025-03-28 22:48:11 +0100

    Removed `PersistedGroup` record
    Eliminated unused class to streamline the codebase.

--- per-worlds/src/main/java/net/thenextlvl/perworlds/model/PersistedGroup.java
+++
@@ -1,9 +0,0 @@
-package net.thenextlvl.perworlds.model;
-
-import net.kyori.adventure.key.Key;
-import net.thenextlvl.perworlds.GroupSettings;
-
-import java.util.Set;
-
-public record PersistedGroup(Set<Key> worlds, GroupSettings settings) {
-}
