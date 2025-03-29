eca518f62a1b20d7e1bdaaa4d5e3f87a30ec92f9
david <github@nonswag.dev>
2025-03-29 18:26:35 +0100

    Enhanced world management plugin detection logic
    Replaced single plugin check with a comprehensive set of known world manager plugins for better metrics reporting.

--- per-worlds/src/main/java/net/thenextlvl/perworlds/SharedWorlds.java
+++ per-worlds/src/main/java/net/thenextlvl/perworlds/SharedWorlds.java
@@ -7,6 +7,8 @@ import org.bstats.charts.SimplePie;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.ServicePriority;
 
+import java.util.Set;
+
 public class SharedWorlds {
     public static final String ISSUES = "https://github.com/TheNextLvl-net/worlds/issues/new?template=bug_report.yml";
     private final PaperGroupProvider groupProvider;
@@ -48,10 +50,25 @@ public class SharedWorlds {
         plugin.getServer().getServicesManager().register(GroupProvider.class, groupProvider, plugin, ServicePriority.Highest);
     }
 
+    private static final Set<String> knownWorldManagers = Set.of( // list ordered by likelihood of a plugin being used
+            "Worlds",
+            "Multiverse-Core", // https://github.com/Multiverse/Multiverse-Core/
+            "MultiWorld", // https://dev.bukkit.org/projects/multiworld-v-2-0 // https://modrinth.com/plugin/multiworld-bukkit
+            "PhantomWorlds", // https://github.com/TheNewEconomy/PhantomWorlds
+            "Hyperverse", // https://github.com/Incendo/Hyperverse
+            "LightWorlds", // https://github.com/justin0-0/LightWorlds
+            "SolarSystem", // https://github.com/OneLiteFeatherNET/SolarSystemPlugin
+            "MoreFoWorld", // https://github.com/Folia-Inquisitors/MoreFoWorld
+            "WorldManager", // https://www.spigotmc.org/resources/worldmanager-1-8-1-18-free-download-api.101875/
+            "WorldMaster", // https://www.spigotmc.org/resources/worldmaster.101171/
+            "TheGalaxyLimits", // https://hangar.papermc.io/TheGlitchedVirus/thegalaxylimits
+            "BulMultiverse", // https://github.com/BulPlugins/BulMultiverse
+            "worldmgr" // https://dev.bukkit.org/projects/worldmgr
+    );
+
     private void addCustomCharts() {
-        metrics.addCustomChart(new SimplePie("using_worlds", () -> {
-            var worlds = plugin.getServer().getPluginManager().getPlugin("Worlds") != null;
-            return String.valueOf(worlds);
-        }));
+        metrics.addCustomChart(new SimplePie("world_management_plugin", () -> knownWorldManagers.stream()
+                .filter(name -> plugin.getServer().getPluginManager().getPlugin(name) != null)
+                .findAny().orElse("None")));
     }
 }
