44d3fd31397c266bf98ec64f60f6424c9ecb3ff4
david <github@nonswag.dev>
2025-04-06 14:50:20 +0200

    Add `group info` command for displaying group details
    Implemented a new command to show information about a group's worlds, with localization support.

--- per-worlds/src/main/java/net/thenextlvl/perworlds/command/GroupInfoCommand.java
+++ per-worlds/src/main/java/net/thenextlvl/perworlds/command/GroupInfoCommand.java
@@ -1,12 +1,34 @@
 package net.thenextlvl.perworlds.command;
 
+import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.tag.resolver.Formatter;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.thenextlvl.perworlds.SharedWorlds;
+import net.thenextlvl.perworlds.WorldGroup;
+
+import static net.thenextlvl.perworlds.command.GroupCommand.groupArgument;
 
 class GroupInfoCommand {
     public static ArgumentBuilder<CommandSourceStack, ?> create(SharedWorlds commons) {
-        return Commands.literal("create");
+        return Commands.literal("info")
+                .requires(source -> source.getSender().hasPermission("perworlds.command.group.info"))
+                .then(groupArgument(commons).executes(context -> info(context, commons)));
+    }
+
+    private static int info(CommandContext<CommandSourceStack> context, SharedWorlds commons) {
+        var sender = context.getSource().getSender();
+        var group = context.getArgument("group", WorldGroup.class);
+        var worlds = group.getWorlds().stream().map(world -> Component.text(world.getName())).toList();
+        commons.bundle().sendMessage(sender, "group.info",
+                Formatter.booleanChoice("single", worlds.size() == 1),
+                Formatter.joining("worlds", worlds),
+                Formatter.number("amount", worlds.size()),
+                Placeholder.unparsed("group", group.getName()));
+        return Command.SINGLE_SUCCESS;
     }
 }
