d0014ffe647309ff1b2ef1d0b7dac0b9247754a0
david <github@nonswag.dev>
2025-04-15 16:44:34 +0200

    Prevent duplicate group creation in SharedWorlds

--- per-worlds/src/main/java/net/thenextlvl/perworlds/SharedWorlds.java
+++ per-worlds/src/main/java/net/thenextlvl/perworlds/SharedWorlds.java
@@ -51,7 +51,7 @@ public class SharedWorlds {
         if (files != null) for (var file : files) {
             var name = file.getName();
             name = name.substring(0, name.length() - suffix.length());
-            groupProvider.createGroup(name);
+            if (!groupProvider.hasGroup(name)) groupProvider.createGroup(name);
         }
     }
 
