0010a5bbe6b6e11383c3d7e7194078ff632ef7f9
david <github@nonswag.dev>
2025-04-24 16:27:17 +0200

    Handle game rule changes and improve world initialization logic
    Added handling for `WorldGameRuleChangeEvent` to sync game rule changes across worlds in a group. Improved world initialization to assign unowned worlds to a default group.

--- per-worlds/src/main/java/net/thenextlvl/perworlds/listener/WorldListener.java
+++ per-worlds/src/main/java/net/thenextlvl/perworlds/listener/WorldListener.java
@@ -1,11 +1,17 @@
 package net.thenextlvl.perworlds.listener;
 
+import io.papermc.paper.event.world.WorldGameRuleChangeEvent;
+import net.thenextlvl.perworlds.WorldGroup;
 import net.thenextlvl.perworlds.group.PaperGroupProvider;
+import org.bukkit.GameRule;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldInitEvent;
 import org.jspecify.annotations.NullMarked;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @NullMarked
 public class WorldListener implements Listener {
@@ -17,7 +23,34 @@ public class WorldListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onWorldInit(WorldInitEvent event) {
-        provider.getGroup(event.getWorld()).ifPresent(group ->
-                group.updateWorldData(event.getWorld()));
+        provider.getGroup(event.getWorld())
+                .orElse(provider.getUnownedWorldGroup())
+                .updateWorldData(event.getWorld());
+    }
+
+    // todo: there is no difficulty change event????
+    // private final Set<WorldGroup> processingDifficulty = new HashSet<>();
+    private final Set<WorldGroup> processingGameRules = new HashSet<>();
+    // todo: process time and weather changes
+    private final Set<WorldGroup> processingWeather = new HashSet<>();
+    private final Set<WorldGroup> processingTime = new HashSet<>();
+
+    @SuppressWarnings("unchecked")
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onWorldGameRuleChange(WorldGameRuleChangeEvent event) {
+        var group = provider.getGroup(event.getWorld())
+                .orElse(provider.getUnownedWorldGroup());
+        if (!processingGameRules.add(group)) return;
+        var gameRule = (GameRule<Object>) event.getGameRule();
+        var value = parseValue(gameRule, event.getValue());
+        group.getGroupData().gameRule(gameRule, value);
+        group.getWorlds().stream()
+                .filter(world -> !world.equals(event.getWorld()))
+                .forEach(group::updateWorldData);
+        processingGameRules.remove(group);
+    }
+
+    private Object parseValue(GameRule<?> rule, String value) {
+        return rule.getType().equals(Integer.class) ? Integer.parseInt(value) : Boolean.parseBoolean(value);
     }
 }
