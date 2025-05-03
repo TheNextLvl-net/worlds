ecf5122c05bdc21144f17dbcb096610964a0839b
david <github@nonswag.dev>
2025-05-03 19:41:15 +0200

    Added event handlers to `WorldRegenerateEvent`
    Introduced `HandlerList` implementation for event management.

--- worlds-api/src/main/java/net/thenextlvl/worlds/api/event/WorldRegenerateEvent.java
+++ worlds-api/src/main/java/net/thenextlvl/worlds/api/event/WorldRegenerateEvent.java
@@ -1,13 +1,25 @@
 package net.thenextlvl.worlds.api.event;
 
 import org.bukkit.World;
+import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 public class WorldRegenerateEvent extends WorldDeleteEvent {
+    private static final HandlerList handlerList = new HandlerList();
+
     @ApiStatus.Internal
     public WorldRegenerateEvent(World world) {
         super(world);
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlerList;
     }
 }
