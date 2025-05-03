2c29758a25dd2111ae73232bcc7b1ed262ed27c0
david <github@nonswag.dev>
2025-05-03 19:43:43 +0200

    Made `WorldDeleteEvent` cancellable
    Added `Cancellable` implementation to `WorldDeleteEvent`.

--- worlds-api/src/main/java/net/thenextlvl/worlds/api/event/WorldDeleteEvent.java
+++ worlds-api/src/main/java/net/thenextlvl/worlds/api/event/WorldDeleteEvent.java
@@ -1,18 +1,31 @@
 package net.thenextlvl.worlds.api.event;
 
 import org.bukkit.World;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.world.WorldEvent;
 import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
-public class WorldDeleteEvent extends WorldEvent {
+public class WorldDeleteEvent extends WorldEvent implements Cancellable {
     private static final HandlerList handlerList = new HandlerList();
+
+    private boolean cancelled = false;
 
     @ApiStatus.Internal
     public WorldDeleteEvent(World world) {
         super(world, false);
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
     }
 
     @Override
