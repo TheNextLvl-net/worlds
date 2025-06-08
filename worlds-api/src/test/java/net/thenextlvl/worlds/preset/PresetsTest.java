5981dd7528c9ad729cc32a883d1c36752bb10aa4
david <github@nonswag.dev>
2025-06-08 17:52:34 +0200

    Refactored preset argument generation in `PresetsTest`
    Replaced `Arguments.argumentSet` calls with `Map.entry` for a cleaner and more concise definition. Improved readability and structure of preset test cases.

--- worlds-api/src/test/java/net/thenextlvl/worlds/preset/PresetsTest.java
+++ worlds-api/src/test/java/net/thenextlvl/worlds/preset/PresetsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,15 +22,15 @@ public class PresetsTest {
 
     public static Stream<Arguments> unparsedPresets() {
         return Stream.of(
-                Arguments.argumentSet("classic flat", Presets.CLASSIC_FLAT, "minecraft:bedrock,2*minecraft:dirt,minecraft:grass_block;minecraft:plains"),
-                Arguments.argumentSet("tunnelers dream", Presets.TUNNELERS_DREAM, "minecraft:bedrock,230*minecraft:stone,5*minecraft:dirt,minecraft:grass_block;minecraft:windswept_hills"),
-                Arguments.argumentSet("water world", Presets.WATER_WORLD, "minecraft:bedrock,64*minecraft:deepslate,5*minecraft:stone,5*minecraft:dirt,5*minecraft:gravel,90*minecraft:water;minecraft:deep_ocean"),
-                Arguments.argumentSet("overworld", Presets.OVERWORLD, "minecraft:bedrock,59*minecraft:stone,3*minecraft:dirt,minecraft:grass_block;minecraft:plains"),
-                Arguments.argumentSet("snowy kingdom", Presets.SNOWY_KINGDOM, "minecraft:bedrock,59*minecraft:stone,3*minecraft:dirt,minecraft:grass_block,minecraft:snow;minecraft:snowy_plains"),
-                Arguments.argumentSet("bottomless pit", Presets.BOTTOMLESS_PIT, "2*minecraft:cobblestone,3*minecraft:dirt,minecraft:grass_block;minecraft:plains"),
-                Arguments.argumentSet("desert", Presets.DESERT, "minecraft:bedrock,3*minecraft:stone,52*minecraft:sandstone,8*minecraft:sand;minecraft:desert"),
-                Arguments.argumentSet("redstone ready", Presets.REDSTONE_READY, "minecraft:bedrock,3*minecraft:stone,116*minecraft:sandstone;minecraft:desert"),
-                Arguments.argumentSet("the void", Presets.THE_VOID, "minecraft:air;minecraft:the_void")
-        );
+                Map.entry(Presets.CLASSIC_FLAT, "minecraft:bedrock,2*minecraft:dirt,minecraft:grass_block;minecraft:plains"),
+                Map.entry(Presets.TUNNELERS_DREAM, "minecraft:bedrock,230*minecraft:stone,5*minecraft:dirt,minecraft:grass_block;minecraft:windswept_hills"),
+                Map.entry(Presets.WATER_WORLD, "minecraft:bedrock,64*minecraft:deepslate,5*minecraft:stone,5*minecraft:dirt,5*minecraft:gravel,90*minecraft:water;minecraft:deep_ocean"),
+                Map.entry(Presets.OVERWORLD, "minecraft:bedrock,59*minecraft:stone,3*minecraft:dirt,minecraft:grass_block;minecraft:plains"),
+                Map.entry(Presets.SNOWY_KINGDOM, "minecraft:bedrock,59*minecraft:stone,3*minecraft:dirt,minecraft:grass_block,minecraft:snow;minecraft:snowy_plains"),
+                Map.entry(Presets.BOTTOMLESS_PIT, "2*minecraft:cobblestone,3*minecraft:dirt,minecraft:grass_block;minecraft:plains"),
+                Map.entry(Presets.DESERT, "minecraft:bedrock,3*minecraft:stone,52*minecraft:sandstone,8*minecraft:sand;minecraft:desert"),
+                Map.entry(Presets.REDSTONE_READY, "minecraft:bedrock,3*minecraft:stone,116*minecraft:sandstone;minecraft:desert"),
+                Map.entry(Presets.THE_VOID, "minecraft:air;minecraft:the_void")
+        ).map(entry -> Arguments.argumentSet(entry.getKey().name(), entry.getKey(), entry.getValue()));
     }
 }
