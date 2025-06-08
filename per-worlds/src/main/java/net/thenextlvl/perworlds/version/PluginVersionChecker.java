f5321c2626247edac44c0c8ad5fb007e583edfd9
david <github@nonswag.dev>
2025-06-08 21:16:42 +0200

    Replaced Hangar version checker with Modrinth API
    Updated `PluginVersionChecker` classes to utilize `PaperModrinthVersionChecker`. Adjusted constructor arguments and added `getLoader` override in the "Worlds" plugin for compatibility with `RUNNING_FOLIA`.

--- per-worlds/src/main/java/net/thenextlvl/perworlds/version/PluginVersionChecker.java
+++ per-worlds/src/main/java/net/thenextlvl/perworlds/version/PluginVersionChecker.java
@@ -1,14 +1,14 @@
 package net.thenextlvl.perworlds.version;
 
-import core.paper.version.PaperHangarVersionChecker;
+import core.paper.version.PaperModrinthVersionChecker;
 import core.version.SemanticVersion;
 import org.bukkit.plugin.Plugin;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
-public class PluginVersionChecker extends PaperHangarVersionChecker<SemanticVersion> {
+public class PluginVersionChecker extends PaperModrinthVersionChecker<SemanticVersion> {
     public PluginVersionChecker(Plugin plugin) {
-        super(plugin, "TheNextLvl", "PerWorlds");
+        super(plugin, "lpfQmSV2");
     }
 
     @Override
--- worlds/src/main/java/net/thenextlvl/worlds/version/PluginVersionChecker.java
+++ worlds/src/main/java/net/thenextlvl/worlds/version/PluginVersionChecker.java
@@ -1,14 +1,20 @@
 package net.thenextlvl.worlds.version;
 
-import core.paper.version.PaperHangarVersionChecker;
+import core.paper.version.PaperModrinthVersionChecker;
 import core.version.SemanticVersion;
+import net.thenextlvl.worlds.WorldsPlugin;
 import org.bukkit.plugin.Plugin;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
-public class PluginVersionChecker extends PaperHangarVersionChecker<SemanticVersion> {
+public class PluginVersionChecker extends PaperModrinthVersionChecker<SemanticVersion> {
     public PluginVersionChecker(Plugin plugin) {
-        super(plugin, "TheNextLvl", "Worlds");
+        super(plugin, "gBIw3Gvy");
+    }
+
+    @Override
+    public String getLoader() {
+        return WorldsPlugin.RUNNING_FOLIA ? "folia" : "paper";
     }
 
     @Override
