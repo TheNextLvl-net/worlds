641e3750b6f7c21072479c62530e0c47ad2ffc46
david <github@nonswag.dev>
2025-06-15 12:48:51 +0200

    [ci skip] Removed unnecessary suppression annotation

--- worlds/src/main/java/net/thenextlvl/worlds/view/PaperLevelView.java
+++ worlds/src/main/java/net/thenextlvl/worlds/view/PaperLevelView.java
@@ -160,7 +160,6 @@ public class PaperLevelView implements LevelView {
      * @see net.minecraft.server.level.ServerLevel#saveLevelData(boolean)
      */
     @Override
-    @SuppressWarnings("JavadocReference")
     public void saveLevelData(World world, boolean async) {
         var level = ((CraftWorld) world).getHandle();
         if (level.getDragonFight() != null) {
