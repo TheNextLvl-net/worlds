f36c67dc6589a848c92e834b67d98faec9c713ce
david <github@nonswag.dev>
2025-07-28 20:54:07 +0200

    Extracted PerWorlds into its own repository

--- per-worlds-api/src/main/java/net/thenextlvl/perworlds/statistics/ItemTypeStat.java
+++
@@ -1,8 +0,0 @@
-package net.thenextlvl.perworlds.statistics;
-
-import org.bukkit.inventory.ItemType;
-import org.jspecify.annotations.NullMarked;
-
-@NullMarked
-public interface ItemTypeStat extends Stat<ItemType> {
-}
