60a9ea495f21a4ee4b262c0cbfad5a82ce63789a
david <github@nonswag.dev>
2025-08-12 21:51:42 +0200

    Added name support to `GeneratorType`
    Extended `GeneratorType` with a new `name` attribute for improved identification. Updated related methods and commands to utilize `name` instead of `key` where applicable.

--- api/src/main/java/net/thenextlvl/worlds/api/generator/GeneratorType.java
+++ api/src/main/java/net/thenextlvl/worlds/api/generator/GeneratorType.java
@@ -13,42 +13,42 @@ import java.util.Optional;
  */
 @NullMarked
 @ApiStatus.Experimental
-public record GeneratorType(Key key) implements Keyed {
+public record GeneratorType(Key key, String name) implements Keyed {
     /**
      * Represents the "amplified" generator type.
      * <a href="https://minecraft.wiki/w/Amplified">Wiki</a>
      */
-    public static final GeneratorType AMPLIFIED = new GeneratorType(Key.key("minecraft", "amplified"));
+    public static final GeneratorType AMPLIFIED = new GeneratorType(Key.key("minecraft", "amplified"), "amplified");
 
     /**
      * Represents the "debug" generator type.
      * <a href="https://minecraft.wiki/w/Dimension_definition#debug">Wiki</a>
      */
-    public static final GeneratorType DEBUG = new GeneratorType(Key.key("minecraft", "debug"));
+    public static final GeneratorType DEBUG = new GeneratorType(Key.key("minecraft", "debug"), "debug");
 
     /**
      * Represents the "flat" generator type.
      * <a href="https://minecraft.wiki/w/Dimension_definition#flat">Wiki</a>
      */
-    public static final GeneratorType FLAT = new GeneratorType(Key.key("minecraft", "flat"));
+    public static final GeneratorType FLAT = new GeneratorType(Key.key("minecraft", "flat"), "flat");
 
     /**
      * Represents the "large_biomes" generator type.
      * <a href="https://minecraft.wiki/w/Large_Biomes">Wiki</a>
      */
-    public static final GeneratorType LARGE_BIOMES = new GeneratorType(Key.key("minecraft", "large_biomes"));
+    public static final GeneratorType LARGE_BIOMES = new GeneratorType(Key.key("minecraft", "large_biomes"), "large-biomes");
 
     /**
      * Represents the "noise" generator type.
      * <a href="https://minecraft.wiki/w/Dimension_definition#noise">Wiki</a>
      */
-    public static final GeneratorType NORMAL = new GeneratorType(Key.key("minecraft", "noise"));
+    public static final GeneratorType NORMAL = new GeneratorType(Key.key("minecraft", "noise"), "normal");
 
     /**
      * Represents the "noise" generator type.
      * <a href="https://minecraft.wiki/w/Dimension_definition#fixed">Wiki</a>
      */
-    public static final GeneratorType SINGLE_BIOME = new GeneratorType(Key.key("minecraft", "fixed"));
+    public static final GeneratorType SINGLE_BIOME = new GeneratorType(Key.key("minecraft", "fixed"), "single-biome");
 
     @ApiStatus.Internal
     @Contract(pure = true)
@@ -60,13 +60,13 @@ public record GeneratorType(Key key) implements Keyed {
     }
 
     @Contract(pure = true)
-    public static Optional<GeneratorType> getByKey(Key key) {
-        if (key.equals(AMPLIFIED.key())) return Optional.of(AMPLIFIED);
-        if (key.equals(DEBUG.key())) return Optional.of(DEBUG);
-        if (key.equals(FLAT.key())) return Optional.of(FLAT);
-        if (key.equals(LARGE_BIOMES.key())) return Optional.of(LARGE_BIOMES);
-        if (key.equals(NORMAL.key())) return Optional.of(NORMAL);
-        if (key.equals(SINGLE_BIOME.key())) return Optional.of(SINGLE_BIOME);
+    public static Optional<GeneratorType> getByName(String name) {
+        if (name.equals(AMPLIFIED.name())) return Optional.of(AMPLIFIED);
+        if (name.equals(DEBUG.name())) return Optional.of(DEBUG);
+        if (name.equals(FLAT.name())) return Optional.of(FLAT);
+        if (name.equals(LARGE_BIOMES.name())) return Optional.of(LARGE_BIOMES);
+        if (name.equals(NORMAL.name())) return Optional.of(NORMAL);
+        if (name.equals(SINGLE_BIOME.name())) return Optional.of(SINGLE_BIOME);
         return Optional.empty();
     }
 }
