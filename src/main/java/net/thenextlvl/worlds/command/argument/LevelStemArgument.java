6e643ff478f02b24915b035080df9e0a47de38bc
david <github@nonswag.dev>
2025-08-27 21:44:46 +0200

    Refactored argument and suggestion providers
    Replaced wrapped argument types and suggestion providers with `SimpleArgumentType` for consistency and modularity. Simplified the implementation by introducing shared methods and enhancing maintainability.

--- src/main/java/net/thenextlvl/worlds/command/argument/LevelStemArgument.java
+++ src/main/java/net/thenextlvl/worlds/command/argument/LevelStemArgument.java
@@ -1,26 +1,48 @@
 package net.thenextlvl.worlds.command.argument;
 
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
-import core.paper.command.WrappedArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.thenextlvl.worlds.WorldsPlugin;
 import net.thenextlvl.worlds.api.generator.LevelStem;
 import net.thenextlvl.worlds.command.suggestion.DimensionSuggestionProvider;
 import org.jspecify.annotations.NullMarked;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @NullMarked
-public final class LevelStemArgument extends WrappedArgumentType<String, LevelStem> {
+public final class LevelStemArgument implements SimpleArgumentType<LevelStem, String> {
+    private final WorldsPlugin plugin;
+
     public LevelStemArgument(WorldsPlugin plugin) {
-        super(StringArgumentType.word(), (reader, type) -> switch (type) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public LevelStem convert(StringReader reader, String type) {
+        return switch (type) {
             case "normal", "overworld" -> LevelStem.OVERWORLD;
             case "nether" -> LevelStem.NETHER;
             case "end" -> LevelStem.END;
             default -> throw new IllegalArgumentException("Custom dimensions are not yet supported");
-        }, new DimensionSuggestionProvider(plugin, Map.of(
+        };
+    }
+
+    @Override
+    public ArgumentType<String> getNativeType() {
+        return StringArgumentType.word();
+    }
+
+    @Override
+    public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+        return new DimensionSuggestionProvider<S>(plugin, Map.of(
                 "normal", "environment.normal",
                 "nether", "environment.nether",
                 "end", "environment.end"
-        )));
+        )).getSuggestions(context, builder);
     }
 }
