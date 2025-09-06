176553c20119d5dc09864cf487e7ee09503b03ed
david <github@nonswag.dev>
2025-09-06 20:52:30 +0200

    Reordered dependencies in build script
    Adjusted dependency order for better clarity and logical grouping.

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,8 +34,8 @@ dependencies {
     implementation(project(":api"))
 
     implementation("net.thenextlvl.core:i18n:3.2.1")
-    implementation("net.thenextlvl.core:paper:2.3.0-pre4")
     implementation("net.thenextlvl:nbt:3.0.0-pre1")
+    implementation("net.thenextlvl.core:paper:2.3.0-pre4")
 
     implementation("org.bstats:bstats-bukkit:3.1.1-SNAPSHOT")
 
