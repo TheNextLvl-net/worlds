b400d07de3eec17a3b09d4481c019cde0017050f
david <github@nonswag.dev>
2025-09-21 15:57:01 +0200

    Update dependencies and game versions for 1.21.9
    - Switch to `paperweight.paperDevBundle` for version 1.21.9-pre2-R0.1-SNAPSHOT
    - Exclude specific `adventure` dependencies from `i18n`
    - Add 1.21.9 to supported game versions in gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,11 +29,15 @@ repositories {
 }
 
 dependencies {
-    paperweight.foliaDevBundle("1.21.9-pre2-R0.1-SNAPSHOT")
+    // paperweight.foliaDevBundle("1.21.9-pre2-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.9-pre2-R0.1-SNAPSHOT")
 
     implementation(project(":api"))
 
-    implementation("net.thenextlvl.core:i18n:3.2.2")
+    implementation("net.thenextlvl.core:i18n:3.2.2") {
+        exclude("net.kyori", "adventure-text-logger-slf4j")
+        exclude("net.kyori", "adventure-text-minimessage")
+    }
     implementation("net.thenextlvl:nbt:3.0.1")
     implementation("net.thenextlvl.core:paper:2.3.1")
 
