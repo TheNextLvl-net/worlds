be925c5b70e7efa55d83b754c271add0961d14ee
david <mail@nonswag.dev>
2025-12-21 17:28:50 +0100

    Updated metrics Bukkit dependency to 0.5.1
    (cherry picked from commit 2ccb822226465e08a2bc889321ef266e841a54d0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
 
     implementation(project(":api"))
 
+    implementation("dev.faststats.metrics:bukkit:0.5.1")
+    implementation("net.thenextlvl.core:files:4.0.0-pre1")
     implementation("net.thenextlvl.version-checker:modrinth-paper:1.0.0")
     implementation("net.thenextlvl:i18n:1.1.0")
     implementation("net.thenextlvl:nbt:4.0.2")
