f2cad42f37406acaae5801406a0e478bdd672a97
david <mail@nonswag.dev>
2025-12-21 05:03:05 +0100

    Updated NBT library version to 4.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation("net.thenextlvl.core:files:4.0.0-pre1")
     implementation("net.thenextlvl.version-checker:modrinth-paper:1.0.0")
     implementation("net.thenextlvl:i18n:1.1.0")
-    implementation("net.thenextlvl:nbt:4.0.1")
+    implementation("net.thenextlvl:nbt:4.0.2")
 
     implementation("org.bstats:bstats-bukkit:3.1.0")
 
