0f002eb14e8dc318ba9c08a233c4d727af649ea9
david <mail@nonswag.dev>
2025-12-28 06:37:54 +0100

    Refactored preset file reading with JsonParser
    (cherry picked from commit 27de250b7f1716f4a6b5cefb5e7fed661579551b)

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,6 @@ dependencies {
 
     implementation(project(":api"))
 
-    implementation("dev.faststats.metrics.java-21:bukkit:0.7.0")
-    implementation("net.thenextlvl.core:files:4.0.0-pre1")
     implementation("net.thenextlvl.version-checker:modrinth-paper:1.0.0")
     implementation("net.thenextlvl:i18n:1.1.0")
     implementation("net.thenextlvl:nbt:4.0.2")
