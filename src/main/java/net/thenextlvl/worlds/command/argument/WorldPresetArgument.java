f4473233584073d93c3432e2ca2806cff934f448
david <mail@nonswag.dev>
2025-12-28 06:37:54 +0100

    Refactored preset file reading with JsonParser
    (cherry picked from commit 27de250b7f1716f4a6b5cefb5e7fed661579551b)

--- src/main/java/net/thenextlvl/worlds/command/argument/WorldPresetArgument.java
+++ src/main/java/net/thenextlvl/worlds/command/argument/WorldPresetArgument.java
@@ -8,7 +8,6 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import core.file.formats.JsonFile;
 import net.thenextlvl.worlds.WorldsPlugin;
 import net.thenextlvl.worlds.api.preset.Preset;
 import net.thenextlvl.worlds.api.preset.Presets;
