ba83d11c72e71ea94d743b0a4c744616386e4ad3
david <mail@nonswag.dev>
2026-01-03 17:17:55 +0100

    Updated version-checker dependency to 1.0.1
    (cherry picked from commit 1b676ad04df6139ac44dab08e4d10fe324fc3ee3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
 
     implementation(project(":api"))
 
-    implementation("net.thenextlvl.version-checker:modrinth-paper:1.0.0")
+    implementation("net.thenextlvl.version-checker:modrinth-paper:1.0.1")
     implementation("net.thenextlvl:i18n:1.1.0")
     implementation("net.thenextlvl:nbt:4.0.0-pre1")
 
