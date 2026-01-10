ceae2061b398a1fa6066ea87883418ad36ee8db6
david <mail@nonswag.dev>
2026-01-10 09:09:10 +0100

    Updated API version to 1.21.9 in build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,7 +63,7 @@ tasks.shadowJar {
 paper {
     name = "Worlds"
     main = "net.thenextlvl.worlds.WorldsPlugin"
-    apiVersion = "1.21.5"
+    apiVersion = "1.21.9"
     description = "Create, delete and manage your worlds"
     load = BukkitPluginDescription.PluginLoadOrder.STARTUP
     website = "https://thenextlvl.net"
