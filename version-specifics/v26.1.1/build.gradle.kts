9a4b2c7ed41c0e68a9fbb9983d02af673e5c2b6c
david <mail@nonswag.dev>
2026-04-06 12:22:16 +0200

    Removed completed todo

--- version-specifics/v26.1.1/build.gradle.kts
+++ version-specifics/v26.1.1/build.gradle.kts
@@ -2,10 +2,6 @@ java {
     toolchain.languageVersion = JavaLanguageVersion.of(25)
 }
 
-// tasks.compileJava { // todo: test on java 25 and java 21
-//     options.release.set(25)
-// }
-
 dependencies {
     paperweight.paperDevBundle("26.1.1.build.20-alpha")
 }
