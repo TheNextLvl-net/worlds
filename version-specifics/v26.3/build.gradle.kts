5d078eaf839412deae914be4d70b00d046cb0cb7
david <mail@nonswag.dev>
2026-09-23 21:00:27 +0200

    Paper 26.3 support

---
+++ version-specifics/v26.3/build.gradle.kts
@@ -0,0 +1,3 @@
+dependencies {
+    paperweight.paperDevBundle("26.3.+")
+}
---
+++ version-specifics/v26.3/src/main/java/net/thenextlvl/worlds/versions/v26_3/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package net.thenextlvl.worlds.versions.v26_3;
+
+import org.jspecify.annotations.NullMarked;
